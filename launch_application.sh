8b884dd44a069bab128bc656facd23c4c34705e4
Darin London <darin.london@duke.edu>
2016-01-14 13:09:07 -0500

    utility script making it easy to launch the application, db, and redis, migrate, and create the consumer rake task to create the consumer from consumer.env

---
+++ launch_application.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $COMPOSE_FILE ]
+then
+  docker-compose up -d
+  sleep 10
+  docker-compose -f dc-dev.utils.yml run rake db:migrate
+  docker-compose -f dc-dev.utils.yml run consumer
+else
+  docker-compose up -d server
+  sleep 10
+  docker-compose run rake db:migrate
+  docker-compose run consumer
+fi
